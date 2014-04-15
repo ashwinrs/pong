283495ef1d674d2137b838c458ba2c9de8e77c91
Ashwin Ramesh Sidhalinganahalli <ashwinrs@fastmail.in>
2014-04-14 23:33:14 -0600

    Created header files and initializations

---
+++ pong/main.h
@@ -0,0 +1,27 @@
+//
+//  main.h
+//  pong
+//
+//  Created by Ashwin R S on 4/14/14.
+//
+//
+
+#ifndef pong_main_h
+#define pong_main_h
+
+const float FPS = 30;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+
+//bouncer size
+const int BOUNCER_SIZE = 10;
+
+//pad size
+const int PAD_W = 40;
+const int PAD_H = 4;
+
+enum MYKEYS {
+    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+};
+
+#endif
---
+++ pong/thread_example.h
@@ -0,0 +1,14 @@
+//
+//  thread_example.h
+//  pong
+//
+//  Created by Ashwin R S on 4/14/14.
+//
+//
+
+#ifndef __pong__thread_example__
+#define __pong__thread_example__
+
+#include <iostream>
+
+#endif /* defined(__pong__thread_example__) */
